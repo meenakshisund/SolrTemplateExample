89d6d2b7282c795efc204a5a6493421f8786e3e5
meenakshisund <meenakshisundaraja@gmail.com>
2020-03-14 18:38:45 +0530

    Added Swagger, README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+1. docker-compose up
+2. docker exec -it --user=solr docker_solr_1 bin/solr create_core -c Student
+(docker_solr_1 is your container **name** - docker container ls)
+3. To view in swagger, use this URL - http://localhost:8080/swagger-ui.html
+4. api/docs - http://localhost:8080/v2/api-docs
+
+Since, managed-schema is not defined, getAllStudents api resulted error. 
+Have created a dummy student with id as 1 during start up.
+ContextRefreshedEvent is used, so that the student with id=1 is created after all the beans are loaded in container.
--- src/main/java/com/apache/solr/SolrTemplateExample/controller/StudentController.java
+++ src/main/java/com/apache/solr/SolrTemplateExample/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.apache.solr.SolrTemplateExample.controller;
 import com.apache.solr.SolrTemplateExample.model.Student;
 import com.apache.solr.SolrTemplateExample.repo.StudentSolrRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +28,15 @@ public class StudentController {
     @GetMapping("/students/{age}")
     public List<Student> getStudentByAge(@PathVariable("age") Integer age) {
         return studentSolrRepository.findByAge(age);
+    }
+
+    @EventListener
+    public void handleContextRefreshEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println("Context Refresh Event received.");
+        Student first = new Student();
+        first.setId("1");
+        first.setAge(30);
+        first.setSalary(300.0);
+        studentSolrRepository.save(first);
     }
 }
